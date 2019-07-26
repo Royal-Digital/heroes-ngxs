89aafb352f3b04d0628bef3f9e69310c75491b37
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-07-26 17:08:31 +0200

    fixed villains component path name

--- src/app/villains/villains.module.ts
+++ src/app/villains/villains.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
-import { VillainsComponent } from "./container/Villains.component";
+import { VillainsComponent } from "./container/villains.component";
 import { SharedModule } from "../shared/shared.module";
 import { EditVillainComponent } from "./edit-villain/edit-villain.component";
 import { NgxsModule } from "@ngxs/store";
